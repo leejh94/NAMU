d264e3f509669ce2107c26ccb39ab044c910307a
이재훈 <kllk1212@naver.com>
2024-11-07 22:11:15 +0900

    feat: 운영 및 개발용 application.properties 분리 및 Jasypt 암호화 적용

--- src/main/java/com/namu/DemoApplication.java
+++ src/main/java/com/namu/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.namu;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 @MapperScan("com.namu.mapper")
 public class DemoApplication {
 
