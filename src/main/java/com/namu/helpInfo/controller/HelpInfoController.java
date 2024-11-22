92d07777a428cb3c4850c81dfcb2272ed2e82683
leejh94 <kmlo121293@gmail.com>
2024-11-22 18:20:06 +0900

    feat : 리액트 빌드 파일 업데이트

---
+++ src/main/java/com/namu/helpInfo/controller/HelpInfoController.java
@@ -0,0 +1,12 @@
+package com.namu.helpInfo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/helpInfo")
+public class HelpInfoController {
+
+
+
+}
