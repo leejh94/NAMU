18a753807037d17184b5c39e407a03fe1c491916
leejh94 <kmlo121293@gmail.com>
2024-11-29 03:10:51 +0900

    feat : 정적 리소스 핸들링 설정

--- src/main/java/com/namu/common/config/WebConfig.java
+++ src/main/java/com/namu/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -36,6 +37,11 @@ public class WebConfig {
                         .setViewName("forward:/index.html");
             }
 
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/**")
+                        .addResourceLocations("classpath:/static/");
+            }
 
         };
     }
